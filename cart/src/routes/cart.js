ceb7ee1750b8983819e77a5b51638826379fee0f
Jhonathan Feliz <sakurakojisakura008@gmail.com>
2025-09-22 15:49:35 -0400

    hacer todo en el routes y ojo al crear al carrito hay que tener buena logica

---
+++ cart/src/routes/cart.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+//tablas: carrito y carrito_items
+router.get(); //ver el carrito_items
+router.post(); //agregar al carrito_items + crear carrito
+router.put(); //subir la cantidad en 1
+router.put(); //bajar la cantidad en 1
+router.delete(); //eliminar de carrito_items
